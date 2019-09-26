ea74f524bf441eb86da78e73eead9ba012131e6c
Caio <caioymc@live.com>
2019-09-26 15:44:55 +0100

    Implemented addVat / getFullName / makeHalfPrice / countBooks / isInStock and getTotalOrderPrice

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,34 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+function getFullName(name, surname){
+    return name+" "+surname;
+}
+
+function makeHalfPrice(price){
+    return price / 2;
+}
+
+function countBooks(books){
+    return books.length;
+}
+
+function isInStock(book){
+    if(book.quantity >= 1){
+        return true;
+    }
+    return false;
+}
+
+function getTotalOrderPrice(price,quantity){
+    return ((price*quantity)*0.2)+(price*quantity);
+}
+
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    getTotalOrderPrice
 };
